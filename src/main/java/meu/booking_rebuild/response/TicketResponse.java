9fc3d9b5c2d6e9856933d99eb3faa5e406ca5b0f
Nguyen Quoc Dai <nguyenquocdai2k1@gmail.com>
2024-01-09 17:07:34 +0700

    fix code

--- src/main/java/meu/booking_rebuild/response/TicketResponse.java
+++ src/main/java/meu/booking_rebuild/response/TicketResponse.java
@@ -6,17 +6,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
+import java.util.UUID;
+
 //@ResponseBody
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class TicketResponse {
+    private UUID id;
     private String name_trip;
     private String name_customer;
     private String number_phone;
     private String code;
-    private int number_tickets;
     private String address;
-    private List<String> seat;
+    private String seat;
 }
